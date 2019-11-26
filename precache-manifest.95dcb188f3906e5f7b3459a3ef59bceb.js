4a64f02454338555b759b87078785627ca762349
Carlos Alba <soradev4@gmail.com>
2019-11-26 13:26:29 -0800

    Updates

---
+++ precache-manifest.95dcb188f3906e5f7b3459a3ef59bceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b0dafcf62fc4ca4272acb5e00787b7",
+    "url": "/textapp/index.html"
+  },
+  {
+    "revision": "9614dda15d54b71c102f",
+    "url": "/textapp/static/css/2.8542f31d.chunk.css"
+  },
+  {
+    "revision": "aa5151a365426c18ca5f",
+    "url": "/textapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9614dda15d54b71c102f",
+    "url": "/textapp/static/js/2.522abec2.chunk.js"
+  },
+  {
+    "revision": "aa5151a365426c18ca5f",
+    "url": "/textapp/static/js/main.2cfa5a5c.chunk.js"
+  },
+  {
+    "revision": "128c0913b8373e35edb1",
+    "url": "/textapp/static/js/runtime-main.4af7d738.js"
+  }
+]);
